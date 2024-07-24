23cece031d18b19801449b0573d3ebbbe8e847ac
Zarzuke <zankiff2013@gmail.com>
2024-07-24 00:30:36 -0400

    Test Login
    pruebas de ventana de login

--- librerias.py
+++ librerias.py
@@ -1,17 +1,29 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox,ttk
+from test import login
 
-def login(user,password):
-    master_data={'Oak':5101999}
-    for users,keys in master_data.items:
-        if users==user and keys==password:
-            return messagebox.showinfo("Éxito", "Inicio de sesión exitoso")
-        else:
-            return messagebox.showerror("Error", "Credenciales incorrectas")
+def recoger_datos():
+    usuario = entry_usuario.get()
+    contrasena = entry_contrasena.get()
+    if login(usuario, contrasena):
+        messagebox.showinfo("Éxito", "Inicio de sesión exitoso")
+        root.destroy()
+        abrir_ventana()
+
+    else:
+        messagebox.showerror("Error", "Usuario o contraseña incorrectos")
+
+def abrir_ventana():
+    nueva_ventana = tk.Tk()
+    nueva_ventana.title("Nueva Ventana")
+    nueva_ventana.geometry("1024x768")
+    etiqueta = ttk.Label(nueva_ventana, text="¡Hola desde la nueva ventana!")
+    etiqueta.pack()
+
 
 root = tk.Tk()
 root.title("Login")
-
+root.geometry("1024x768")
 label_usuario = tk.Label(root, text="Usuario:")
 label_usuario.pack()
 entry_usuario = tk.Entry(root)
@@ -22,9 +34,10 @@ label_contrasena.pack()
 entry_contrasena = tk.Entry(root, show="*")  # Muestra asteriscos en lugar de la contraseña
 entry_contrasena.pack()
 
-boton_login = tk.Button(root, text="Iniciar sesión", command=login)
+boton_login = tk.Button(root, text="Iniciar sesión", command=recoger_datos)
 boton_login.pack()
 
 root.mainloop()
 
 #hola
+
---
+++ test.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def login(user,password):
+    Teacher='Oak'
+    PassW=5101999
+    return Teacher==user and PassW==int(password)
+
