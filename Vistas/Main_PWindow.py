2bd276e8a6bc63d1dea448fcbf00382f74fb19c9
LeonardoGarcia2005 <120688507+LeonardoGarcia2005@users.noreply.github.com>
2024-07-25 18:34:28 -0500

    Cambios globales

--- Vistas/Main_PWindow.py
+++ Vistas/Main_PWindow.py
@@ -15,14 +15,16 @@ def relative_to_assets(path: str) -> Path:
     return full_path
 
 # Clase principal de la aplicación
-class App(tk.Tk):
+class Balls(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Pokedex")
         self.geometry("1366x768")
         self.container = tk.Frame(self)
+        self.resizable(False, False)  # No permitir cambiar el tamaño de la ventana
         self.container.pack(fill="both", expand=True)
         self.current_frame = None
+        self.iconbitmap(relative_to_assets('PokeBall.ico'))
         self.show_frame(SecondaryPage)
 
     def show_frame(self, page_class):
@@ -76,6 +78,7 @@ class Main_PW(tk.Frame):
             image=self.images["button_volver"],
             borderwidth=0,
             highlightthickness=0,
+            activebackground="#E83030",
             command=lambda: self.open_seleccion(),
             relief="flat"
         ).place(x=1135.0, y=15.0, width=208.0, height=29.0)
@@ -154,148 +157,6 @@ class Main_PW(tk.Frame):
             font=("Inter", 14 * -1)
         )
 
-class Eliminar(tk.Frame):
-    def __init__(self, parent, controller):
-        super().__init__(parent)
-        self.controller = controller
-        usuario=recoger_sesion()
-        self.usuario=usuario
-        self.config(bg="#FFFFFF")
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.images = {}  # Diccionario para almacenar las imágenes
-
-        def relative_to_assets(path: str) -> Path:
-            return ASSETS_PATH / Path(path)
-
-        canvas = tk.Canvas(
-            self,
-            bg="#FFFFFF",
-            height=768,
-            width=1366,
-            bd=0,
-            highlightthickness=0,
-            relief="ridge"
-        )
-        canvas.place(x=0, y=0)
-
-        # Cargar y almacenar las imágenes de las esquinas
-        self.images["image_1"] = tk.PhotoImage(file=relative_to_assets("main_relleno_lateral.png"))
-        canvas.create_image(107.0, 412.0, image=self.images["image_1"])
-
-        self.images["image_2"] = tk.PhotoImage(file=relative_to_assets("main_relleno_general.png"))
-        canvas.create_image(789.0, 413.0, image=self.images["image_2"])
-
-        self.images["image_3"] = tk.PhotoImage(file=relative_to_assets("main_barra.png"))
-        canvas.create_image(682.0, 29.0, image=self.images["image_3"])
-
-        # Cargar y almacenar las imágenes de los botones
-        self.images["button_image_5"] = tk.PhotoImage(file=relative_to_assets("main_boton_registrar.png"))
-        self.button_registrar = tk.Button(
-            self,
-            image=self.images["button_image_5"],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: {self.hide_widgets(), self.controller.show_frame(Registrar)},
-            relief="flat"
-        )
-        self.button_registrar.place(x=1.0, y=57.0, width=213.0, height=58.0)
-        
-        self.images["button_image_1"] = tk.PhotoImage(file=relative_to_assets("main_boton_volver.png"))
-        self.button_volver = tk.Button(
-            self,
-            image=self.images["button_image_1"],
-            borderwidth=0,
-            highlightthickness=0,
-            relief="flat"
-        )
-        self.button_volver.place(x=1135.0, y=15.0, width=208.0, height=29.0)
-
-        self.images["button_image_2"] = tk.PhotoImage(file=relative_to_assets("main_boton_modificar.png"))
-        self.button_modificar = tk.Button(
-            self,
-            image=self.images["button_image_2"],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: {self.hide_widgets(), self.controller.show_frame(Modificar)},
-            relief="flat"
-        )
-        self.button_modificar.place(x=1.0, y=173.0, width=213.0, height=58.0)
-
-        self.images["button_image_3"] = tk.PhotoImage(file=relative_to_assets("main_boton_listado.png"))
-        self.button_listado = tk.Button(
-            self,
-            image=self.images["button_image_3"],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_3 clicked"),
-            relief="flat"
-        )
-        self.button_listado.place(x=1.0, y=115.0, width=213.0, height=58.0)
-
-        self.images["button_image_4"] = tk.PhotoImage(file=relative_to_assets("main_boton_eliminar.png"))
-        self.button_eliminar = tk.Button(
-            self,
-            image=self.images["button_image_4"],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: {self.controller.show_frame(Eliminar)},
-            relief="flat"
-        )
-        self.button_eliminar.place(x=1.0, y=231.0, width=213.0, height=58.0)
-        
-        # Formulario para el eliminar
-        self.label_info = canvas.create_text(263.0, 106.0, anchor="nw", text="Ingrese la información del pokemon a Eliminar", fill="#4C4C4C", font=("Montserrat Medium", 15))
-        
-        # Texto para el nombre
-        self.label_nombre = canvas.create_text(263.0, 152.0, anchor="nw", text="Nombre", fill="#000000", font=("Montserrat Regular", 15))
-        
-        self.input_nombre = tk.Entry(
-            bd=0,
-            bg="#FFFFFF",
-            fg="#000716",
-            highlightthickness=0, 
-            borderwidth=0.5, 
-            relief="solid"
-        )
-        self.input_nombre.place(x=263.0, y=182.0, width=237.0, height=38.0)
-        
-        # Cargar y almacenar las imágenes
-        self.images['boton_Eliminar_f'] = tk.PhotoImage(file=relative_to_assets("Boton_eliminar.png"))
-        
-        # Cargar y almacenar la imagen del botón
-        self.button_e = tk.Button(
-            image=self.images['boton_Eliminar_f'],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_1 clicked"),
-            relief="flat"
-        )
-        self.button_e.place(x=265.0, y=264.0, width=130.0, height=40.0)
-        
-        # Texto del navbar
-        self.navbar_title = canvas.create_text(226.0, 5.0, anchor="nw", text="Pokédex", fill="#000000", font=("Inter", 40 * -1))
-        self.navbar_subtitle_1 = canvas.create_text(26.0, 9.0, anchor="nw", text=self.usuario, fill="#000000", font=("Inter", 18 * -1))
-        self.navbar_subtitle_2 = canvas.create_text(26.0, 31.0, anchor="nw", text="Admin", fill="#000000", font=("Inter", 14 * -1))
-
-        # Guarda el canvas para poder ocultarlo
-        self.canvas = canvas
-        
-    def hide_widgets(self):
-        self.button_registrar.place_forget()
-        self.button_volver.place_forget()
-        self.button_modificar.place_forget()
-        self.button_listado.place_forget()
-        self.button_eliminar.place_forget()
-        self.input_nombre.place_forget()
-        self.button_e.place_forget()
-        self.canvas.itemconfigure(self.label_info, state='hidden')
-        self.canvas.itemconfigure(self.label_nombre, state='hidden')
-        self.canvas.itemconfigure(self.navbar_title, state='hidden')
-        self.canvas.itemconfigure(self.navbar_subtitle_1, state='hidden')
-        self.canvas.itemconfigure(self.navbar_subtitle_2, state='hidden')
-
 class Registrar(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
@@ -333,6 +194,17 @@ class Registrar(tk.Frame):
         canvas.create_image(682.0, 29.0, image=self.images["image_3"])
 
         # Cargar y almacenar las imagenes
+        self.images["button_volver"] = tk.PhotoImage(file=relative_to_assets("main_boton_volver.png"))
+        tk.Button(
+            self,
+            image=self.images["button_volver"],
+            borderwidth=0,
+            highlightthickness=0,
+            activebackground="#E83030",
+            command=lambda: None,
+            relief="flat"
+        ).place(x=1135.0, y=15.0, width=208.0, height=29.0)
+        
         self.images["button_image_5"] = tk.PhotoImage(file=relative_to_assets("main_boton_registrar.png"))
         tk.Button(
             self,
@@ -342,16 +214,6 @@ class Registrar(tk.Frame):
             command=lambda: {self.hide_widgets(), self.controller.show_frame(Registrar)},
             relief="flat"
         ).place(x=1.0, y=57.0, width=213.0, height=58.0)
-        
-        self.images["button_image_1"] = tk.PhotoImage(file=relative_to_assets("main_boton_volver.png"))
-        tk.Button(
-            self,
-            image=self.images["button_image_1"],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: None,
-            relief="flat"
-        ).place(x=1135.0, y=15.0, width=208.0, height=29.0)
 
         self.images["button_image_2"] = tk.PhotoImage(file=relative_to_assets("main_boton_modificar.png"))
         tk.Button(
@@ -527,6 +389,149 @@ class Registrar(tk.Frame):
         self.canvas.itemconfigure(self.navbar_subtitle_1, state='hidden')
         self.canvas.itemconfigure(self.navbar_subtitle_2, state='hidden')
 
+class Eliminar(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        usuario=recoger_sesion()
+        self.usuario=usuario
+        self.config(bg="#FFFFFF")
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.images = {}  # Diccionario para almacenar las imágenes
+
+        def relative_to_assets(path: str) -> Path:
+            return ASSETS_PATH / Path(path)
+
+        canvas = tk.Canvas(
+            self,
+            bg="#FFFFFF",
+            height=768,
+            width=1366,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
+        canvas.place(x=0, y=0)
+
+        # Cargar y almacenar las imágenes de las esquinas
+        self.images["image_1"] = tk.PhotoImage(file=relative_to_assets("main_relleno_lateral.png"))
+        canvas.create_image(107.0, 412.0, image=self.images["image_1"])
+
+        self.images["image_2"] = tk.PhotoImage(file=relative_to_assets("main_relleno_general.png"))
+        canvas.create_image(789.0, 413.0, image=self.images["image_2"])
+
+        self.images["image_3"] = tk.PhotoImage(file=relative_to_assets("main_barra.png"))
+        canvas.create_image(682.0, 29.0, image=self.images["image_3"])
+
+        # Cargar y almacenar las imágenes de los botones
+        self.images["button_image_5"] = tk.PhotoImage(file=relative_to_assets("main_boton_registrar.png"))
+        self.button_registrar = tk.Button(
+            self,
+            image=self.images["button_image_5"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: {self.hide_widgets(), self.controller.show_frame(Registrar)},
+            relief="flat"
+        )
+        self.button_registrar.place(x=1.0, y=57.0, width=213.0, height=58.0)
+        
+        self.images["button_image_1"] = tk.PhotoImage(file=relative_to_assets("main_boton_volver.png"))
+        self.button_volver = tk.Button(
+            self,
+            image=self.images["button_image_1"],
+            borderwidth=0,
+            activebackground="#E83030",
+            highlightthickness=0,
+            relief="flat"
+        )
+        self.button_volver.place(x=1135.0, y=15.0, width=208.0, height=29.0)
+
+        self.images["button_image_2"] = tk.PhotoImage(file=relative_to_assets("main_boton_modificar.png"))
+        self.button_modificar = tk.Button(
+            self,
+            image=self.images["button_image_2"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: {self.hide_widgets(), self.controller.show_frame(Modificar)},
+            relief="flat"
+        )
+        self.button_modificar.place(x=1.0, y=173.0, width=213.0, height=58.0)
+
+        self.images["button_image_3"] = tk.PhotoImage(file=relative_to_assets("main_boton_listado.png"))
+        self.button_listado = tk.Button(
+            self,
+            image=self.images["button_image_3"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_3 clicked"),
+            relief="flat"
+        )
+        self.button_listado.place(x=1.0, y=115.0, width=213.0, height=58.0)
+
+        self.images["button_image_4"] = tk.PhotoImage(file=relative_to_assets("main_boton_eliminar.png"))
+        self.button_eliminar = tk.Button(
+            self,
+            image=self.images["button_image_4"],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: {self.controller.show_frame(Eliminar)},
+            relief="flat"
+        )
+        self.button_eliminar.place(x=1.0, y=231.0, width=213.0, height=58.0)
+        
+        # Formulario para el eliminar
+        self.label_info = canvas.create_text(263.0, 106.0, anchor="nw", text="Ingrese la información del pokemon a Eliminar", fill="#4C4C4C", font=("Montserrat Medium", 15))
+        
+        # Texto para el nombre
+        self.label_nombre = canvas.create_text(263.0, 152.0, anchor="nw", text="Nombre", fill="#000000", font=("Montserrat Regular", 15))
+        
+        self.input_nombre = tk.Entry(
+            bd=0,
+            bg="#FFFFFF",
+            fg="#000716",
+            highlightthickness=0, 
+            borderwidth=0.5, 
+            relief="solid"
+        )
+        self.input_nombre.place(x=263.0, y=182.0, width=237.0, height=38.0)
+        
+        # Cargar y almacenar las imágenes
+        self.images['boton_Eliminar_f'] = tk.PhotoImage(file=relative_to_assets("Boton_eliminar.png"))
+        
+        # Cargar y almacenar la imagen del botón
+        self.button_e = tk.Button(
+            image=self.images['boton_Eliminar_f'],
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_1 clicked"),
+            relief="flat"
+        )
+        self.button_e.place(x=265.0, y=264.0, width=130.0, height=40.0)
+        
+        # Texto del navbar
+        self.navbar_title = canvas.create_text(226.0, 5.0, anchor="nw", text="Pokédex", fill="#000000", font=("Inter", 40 * -1))
+        self.navbar_subtitle_1 = canvas.create_text(26.0, 9.0, anchor="nw", text=self.usuario, fill="#000000", font=("Inter", 18 * -1))
+        self.navbar_subtitle_2 = canvas.create_text(26.0, 31.0, anchor="nw", text="Admin", fill="#000000", font=("Inter", 14 * -1))
+
+        # Guarda el canvas para poder ocultarlo
+        self.canvas = canvas
+        
+    def hide_widgets(self):
+        self.button_registrar.place_forget()
+        self.button_volver.place_forget()
+        self.button_modificar.place_forget()
+        self.button_listado.place_forget()
+        self.button_eliminar.place_forget()
+        self.input_nombre.place_forget()
+        self.button_e.place_forget()
+        self.canvas.itemconfigure(self.label_info, state='hidden')
+        self.canvas.itemconfigure(self.label_nombre, state='hidden')
+        self.canvas.itemconfigure(self.navbar_title, state='hidden')
+        self.canvas.itemconfigure(self.navbar_subtitle_1, state='hidden')
+        self.canvas.itemconfigure(self.navbar_subtitle_2, state='hidden')
+
 class Modificar(tk.Frame):
     def __init__(self, parent, controller):
         usuario=recoger_sesion()
@@ -564,6 +569,17 @@ class Modificar(tk.Frame):
         canvas.create_image(682.0, 29.0, image=self.images["image_3"])
 
         # Cargar y almacenar las imagenes
+        self.images["button_volver"] = tk.PhotoImage(file=relative_to_assets("main_boton_volver.png"))
+        tk.Button(
+            self,
+            image=self.images["button_volver"],
+            activebackground="#E83030",
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: None,
+            relief="flat"
+        ).place(x=1135.0, y=15.0, width=208.0, height=29.0)
+        
         self.images["button_image_5"] = tk.PhotoImage(file=relative_to_assets("main_boton_registrar.png"))
         tk.Button(
             self,
@@ -573,16 +589,6 @@ class Modificar(tk.Frame):
             command=lambda: {self.controller.show_frame(Registrar)},
             relief="flat"
         ).place(x=1.0, y=57.0, width=213.0, height=58.0)
-        
-        self.images["button_image_1"] = tk.PhotoImage(file=relative_to_assets("main_boton_volver.png"))
-        tk.Button(
-            self,
-            image=self.images["button_image_1"],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: None,
-            relief="flat"
-        ).place(x=1135.0, y=15.0, width=208.0, height=29.0)
 
         self.images["button_image_2"] = tk.PhotoImage(file=relative_to_assets("main_boton_modificar.png"))
         tk.Button(
@@ -757,8 +763,11 @@ class Modificar(tk.Frame):
         self.canvas.itemconfigure(self.navbar_title, state='hidden')
         self.canvas.itemconfigure(self.navbar_subtitle_1, state='hidden')
         self.canvas.itemconfigure(self.navbar_subtitle_2, state='hidden')
-        
+
+class Listado(tk.Frame):
+    pass
+       
 # Crear y ejecutar la aplicación
 if __name__ == "__main__":
-    app = App()
+    app = Balls()
     app.mainloop()
